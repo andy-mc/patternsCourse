9333b47002aa06a905c8ff76a6fe806cde5b1e0c
Andy M. Celi <andres.munoz01@globant.com>
2023-03-10 20:13:44 -0500

    better way of copy

--- myPatterns/prototype/explicit_coying.js
+++ myPatterns/prototype/explicit_coying.js
@@ -5,6 +5,10 @@ class Address {
     this.state = state;
   }
 
+  deepCopy() {
+    return new Address(this.street, this.city, this.state);
+  }
+
   toString() {
     return `${this.street}, ${this.city}, ${this.state}`;
   }
@@ -16,41 +20,32 @@ class Person {
     this.address = address;
   }
 
+  deepCopy() {
+    return new Person(this.name, this.address.deepCopy());
+  }
+
   toString() {
     return `${this.name} lives at ${this.address.toString()}`;
   }
 }
 
 const prototype = new Person('Nico', new Address('123 Main St.', 'New York', 'NY'));
-const person2 = JSON.parse(JSON.stringify(prototype));
-const person3 = JSON.parse(JSON.stringify(prototype));
-person2.toString = function() {
-  return `${this.name} lives at ${this.address.toString()}`;
-}
-person2.address.toString = function() {
-  return `${this.street}, ${this.city}, ${this.state}`;
-}
-person3.toString = function() {
-  return `${this.name} lives at ${this.address.toString()}`;
-}
-person3.address.toString = function() {
-  return `${this.street}, ${this.city}, ${this.state}`;
-}
+const person2 = prototype.deepCopy();
+const person3 = prototype.deepCopy();
 
-console.log('person1:', prototype.toString())
 console.log('person2:', person2.toString())
 console.log('person3:', person3.toString())
-
 
 console.log("----------------------------------------------------")
 
 person2.name = "Andy" 
+person2.address.street = "main st Amsterdam" 
 person2.address.city = "Amsterdam" 
 
 person3.name = "Luigy" 
+person3.address.street = "Coral Gabels st" 
 person3.address.city = "Miami" 
 
-console.log('person1:', prototype.toString())
 console.log('person2:', person2.toString())
 console.log('person3:', person3.toString())
 

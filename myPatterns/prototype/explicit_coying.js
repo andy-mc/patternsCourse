6965ef468079a6434ae7358f5ba31e5dd0c1ef9b
Andy M. Celi <andres.munoz01@globant.com>
2023-03-11 06:24:10 -0500

    implement serializer

--- myPatterns/prototype/explicit_coying.js
+++ myPatterns/prototype/explicit_coying.js
@@ -19,11 +19,63 @@ class Person {
   toString() {
     return `${this.name} lives at ${this.address.toString()}`;
   }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name} I live at ${this.address.toString()}`)
+  }
+}
+
+class Serializer {
+  constructor(types) {
+    this.types = types;
+  }
+
+  markRecursive(object) {
+    let idx = this.types.findIndex(t => t.name === object.constructor.name);
+
+    if (idx !== -1) {
+      object["typeIndex"] = idx;
+
+      for (let prop in object) {
+        if (object.hasOwnProperty(prop)) {
+          this.markRecursive(object[prop]);
+        }
+      }
+
+      return object;
+    }
+
+    return object;
+  }
+
+  reconstructRecursive(object) {
+    if (object.hasOwnProperty('typeIndex')) {
+      const type = this.types[object.typeIndex]
+      const newInstance  = new type();
+           
+      for (let prop in object) {
+        if (object.hasOwnProperty(prop) && prop !== 'typeIndex' && object[prop] !== null) {
+          newInstance[prop] = this.reconstructRecursive(object[prop]);
+        }
+      }
+      
+      return newInstance;
+    }
+    
+    return object;
+  }
+
+  deepCopy(object) {
+    let marked = this.markRecursive(object);
+    let copy = JSON.parse(JSON.stringify(marked));
+    return this.reconstructRecursive(copy);
+  }
 }
 
 const prototype = new Person('Nico', new Address('123 Main St.', 'New York', 'NY'));
-const person2 = prototype.deepCopy();
-const person3 = prototype.deepCopy();
+const serializer = new Serializer([Person, Address]);
+const person2 = serializer.deepCopy(prototype);
+const person3 = serializer.deepCopy(prototype);
 
 console.log('person2:', person2.toString())
 console.log('person3:', person3.toString())
@@ -40,4 +92,4 @@ person3.address.city = "Miami"
 
 console.log('person2:', person2.toString())
 console.log('person3:', person3.toString())
-
+person3.greet()
